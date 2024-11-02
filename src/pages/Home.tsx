930e18054e7633e906119fa1da1a5970199daf4e
Burakkturk <burak1845turk@icloud.com>
2024-11-02 14:42:25 +0300

    get Todos fn added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react"
-
+import { useEffect, useState } from "react"
+import axios from "axios"
 interface TodoType {
   "todo":string ,"isDone":boolean,"id":string | number,
 }
@@ -8,8 +8,23 @@ const Home = () => {
   // const [todos,setTodos] = useState([] as TodoType[])
 
   const [todos,setTodos] =useState<TodoType[]>([])
+const url:string = import.meta.env.VITE_BASE_URL
+const getTodos = async () => {
+try {
+  const {data} = await axios(url)
+  // const {data} = await axios<TodoType[]>(url)
+  console.log(data)
+  setTodos(data)
 
-
+} catch (error) {
+  console.log(error)
+  
+}
+return "merhaba"
+}
+useEffect(() => {
+  getTodos()
+},[])
 
   return (
     <div onClick={() => setTodos([{id:5, todo:"eren", isDone:true}])}>Home</div>
