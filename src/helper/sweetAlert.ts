86fab3327a1d343a3481d0242e4663caf71ec179
Burakkturk <burak1845turk@icloud.com>
2024-11-20 02:35:40 +0300

    finished project

---
+++ src/helper/sweetAlert.ts
@@ -0,0 +1,11 @@
+import swal from "sweetalert";
+
+
+export const notify = (msg:string,color:string) => swal(
+    {
+        title: "Anthony Todo App",
+        text:msg,
+        icon:color,
+        timer: 2000,
+    }
+)
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,70 +1,81 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Container, Typography } from "@mui/material";
-import AddTodoComp  from "../components/AddTodoComp";
-
+import AddTodoComp from "../components/AddTodoComp";
 import TodoList from "../components/TodoList";
-import TodoListItem from "../components/TodoListItem";
+import { notify } from "../helper/sweetAlert";
 // interface TodoType {
 //   todo: string;
 //   isDone: boolean;
-//   id: string | number;
+//   id: string | number; // id bilgisi string yada number olabilir. İki veri tipinide kabul edecek.
 // }
 
 const Home = () => {
-  // const [todos,setTodos] = useState([] as TodoType[])
-
+  // const [todos, setTodos] = useState([] as TodoType[]);
+  // const [todos, setTodos] = useState<Array<TodoType>>([]);
   const [todos, setTodos] = useState<TodoType[]>([]);
   const url: string = import.meta.env.VITE_BASE_URL;
+
   const getTodos = async () => {
     try {
-      const { data } = await axios(url);
-      // const {data} = await axios<TodoType[]>(url)
+      // const {data} = await axios(url)
+      const { data } = await axios<TodoType[]>(url);
       console.log(data);
       setTodos(data);
     } catch (error) {
       console.log(error);
     }
-    // const addTodo = async (text:string) => {
-    //   try {
+  };
 
-    //   } catch (error) {
+  // const addTodo = async (text:string) => {
+  //   try {
 
-    //   }
-    type AddFn = (text: string) => Promise<void>;
-    const addTodo: AddFn = async text => {
-      try {
-        await axios.post(url, { todo: text, isDone: false });
-      } catch (error) {
-        console.log(error);
-      } finally {
-        getTodos();
-      }
-    };
-    const toggleTodo: ToggleFn = async (todo) => {
-      try {
-        await axios.put(`${url}/${todo.id}`, {...todo,isDone: !todo.isDone});
-      } catch (error) {
-        console.log(error);
-      } finally {
-        getTodos();
-      }
-    };
+  //   } catch (error) {}
+  // };
+  type AddFn = (text: string) => Promise<void>;
 
-    const deleteTodo: DeleteFn = async (id) => {
-      try {
-        await axios.delete(`${url}/${id}`);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        getTodos();
-      }
-    };
-  
+  const addTodo: AddFn = async text => {
+    try {
+      await axios.post(url, { todo: text, isDone: false });
+      notify("The todo was created successfully!", "success");
+    } catch (error) {
+      console.log(error);
+      notify("The todo was not created successfully!", "error");
+    } finally {
+      getTodos();
+    }
+  };
+
+  const toggleTodo: ToggleFn = async todo => {
+    try {
+      await axios.put(`${url}/${todo.id}/sdasd`, {
+        ...todo,
+        isDone: !todo.isDone,
+      });
+      notify("The todo was updated successfully!", "success");
+    } catch (error) {
+      console.log(error);
+      notify("The todo was not updated successfully!", "error");
+    } finally {
+      getTodos();
+    }
+  };
+  const deleteTodo: DeleteFn = async id => {
+    try {
+      await axios.delete(`${url}/${id}`);
+      notify("The todo was deleted successfully!", "success");
+    } catch (error) {
+      console.log(error);
+      notify("The todo was not deleted successfully!", "error");
+    } finally {
+      getTodos();
+    }
+  };
+
   useEffect(() => {
     getTodos();
   }, []);
-}
+
   return (
     <Container>
       <Typography
@@ -72,15 +83,11 @@ const Home = () => {
         align="center"
         variant="h2"
         component={"h1"}
-        mt={3}
-      >
-        Todo App With Typescript
+        mt={3}>
+        Todo App with Typescript
       </Typography>
-   
-      <AddTodoComp addTodo={""} />
-      
+      <AddTodoComp addTodo={addTodo} />
       <TodoList todos={todos} deleteTodo={deleteTodo} toggleTodo={toggleTodo} />
-      
     </Container>
   );
 };
